68eccfc271599cbb682d9c97ac857ce6f077ba24
Ayush Chauhan <pingmeorayush@gmail.com>
2022-04-20 02:49:43 +0530

    added articles listing feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
     def show
-        # @ sign before a variable just converts a normal variable to the instance variable
+        # @ sign before a variable just converts a normal variable to the instance variable so that it will be avaiable in the views
         @article = Article.find(params[:id])
     end
 end
