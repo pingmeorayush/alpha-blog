515a03d7d16689b8e74fb47c8a772826d6fb9159
Ayush Chauhan <pingmeorayush@gmail.com>
2022-04-20 05:07:02 +0530

    refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index]
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
