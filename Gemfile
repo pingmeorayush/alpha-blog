a6115dce7ab97e9eeb05ba9a3c53fbe2fa4bbffa
Ayush Chauhan <pingmeorayush@gmail.com>
2022-04-21 13:12:19 +0530

    added bootstrap and styled the components

--- Gemfile
+++ Gemfile
@@ -70,3 +70,6 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "webpacker", "~> 5.4"
+gem "cssbundling-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  root 'pages#home'
   resources :articles
 end
